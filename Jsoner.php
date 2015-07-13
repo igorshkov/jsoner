e0f1bd2adafabbc0470cbab760150c135fd817e2
igorshkov <ivan88gorshkov@gmail.com>
2015-07-13 21:48:42 +0300

    init

---
+++ Jsoner.php
@@ -0,0 +1,14 @@
+<?php
+
+class Jsoner
+{
+    public function __construct()
+    {
+
+    }
+
+    public function init()
+    {
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once('Jsoner.php');
+
+$jsoner = new Jsoner();
